085089806689593e49a39ffa3aee7f26335471a0
saadyousafarbi <saad.yousaf@arbisoft.com>
2019-09-29 21:15:01 +0500

    Add validation for models. Added custom filter for books.

--- books/api/v1/views.py
+++ books/api/v1/views.py
@@ -1,8 +1,9 @@
-import django_filters
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
+from books.api.v1.utils import transform_response
+from books.filters import BooksFilter
 from books.models import Book
 from books.serializers import BookModelSerializer
 
@@ -15,61 +16,47 @@ class BooksViewSet(ModelViewSet):
     lookup_field = 'pk'
     queryset = Book.objects.all()
     serializer_class = BookModelSerializer
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['name', 'country', 'publisher']
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = BooksFilter
 
     def list(self, request, *args, **kwargs):
         """
         Retrieve list of books with/without query_params.
         """
-        queryset = Book.objects.all()
-        serializer = BookModelSerializer(queryset, many=True)
-        return Response({"status_code": 200,
-                         "status": "success",
-                         "data": serializer.data})
+        response = super(BooksViewSet, self).list(request, *args, **kwargs)
+        return Response(data=transform_response(status="success", status_code=response.status_code,
+                                                data=response.data, message=None))
 
     def create(self, request, *args, **kwargs):
         """
         Creates new book for arguments passed in POST request.
         """
-        serializer = BookModelSerializer(data=request.data)
-        serializer.is_valid()
-        serializer.save()
-        return Response({"status_code": 201,
-                         "status": "success",
-                         "data": serializer.data})
+        response = super(BooksViewSet, self).create(request, *args, **kwargs)
+        return Response(data=transform_response(status="success", status_code=response.status_code,
+                                                data=response.data, message=None))
 
     def update(self, request, *args, **kwargs):
         """
         Updates book instance with arguments passed in PATCH request.
         """
-        book_instance = Book.objects.get(id=self.kwargs['pk'])
-        book_name = book_instance.name
-        serializer = BookModelSerializer(book_instance, data=request.data, partial=True)
-        serializer.is_valid()
-        serializer.save()
-        return Response({"status_code": 200,
-                         "status": "success",
-                         "message": "The book {} was updated successfully".format(book_name),
-                         "data": serializer.data})
-
-    def retrieve(self, request, *args, **kwargs):
-        """
-        Retrieves book for specified id from URL params.
-        """
-        instance = self.get_object()
-        serializer = self.get_serializer(instance)
-        return Response({"status_code": 200,
-                         "status": "success",
-                         "data": serializer.data})
+        response = super(BooksViewSet, self).update(request, *args, **kwargs)
+        book_name = self.get_object().name
+        return Response(data=transform_response(status="success", status_code=response.status_code, data=response.data,
+                                                message="The book {} was updated successfully".format(book_name)))
 
     def destroy(self, request, *args, **kwargs):
         """
         Deletes book for specified id from URL params.
         """
-        instance = self.get_object()
-        self.perform_destroy(instance)
-        return Response({"status_code": 204,
-                         "status": "success",
-                         "message": "The book {} was deleted successfully".format(instance.name),
-                         "data": []})
+        book_name = self.get_object().name
+        response = super(BooksViewSet, self).destroy(request, *args, **kwargs)
+        return Response(data=transform_response(status="success", status_code=response.status_code, data=[],
+                                                message="The book {} was deleted successfully".format(book_name)))
+
+    def retrieve(self, request, *args, **kwargs):
+        """
+        Retrieves book for specified id from URL params.
+        """
+        response = super(BooksViewSet, self).retrieve(request, *args, **kwargs)
+        return Response(data=transform_response(status="success", status_code=response.status_code,
+                                                data=response.data, message=None))
