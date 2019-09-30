544e7cf2a0ba12100ae6d2b66d0dc3c78b32be85
saadyousafarbi <saad.yousaf@arbisoft.com>
2019-09-30 15:01:43 +0500

    add test cases for external-book view.

--- books/urls.py
+++ books/urls.py
@@ -3,7 +3,10 @@ from django.urls import include
 
 from books.views import get_books_from_api
 
+app_name = 'api'
+
+
 urlpatterns = [
-    url(r'^external-books/$', get_books_from_api),
-    url('^v1/', include('books.api.v1.urls')),
+    url(r'^external-books/$', get_books_from_api, name='external-books'),
+    url('^v1/', include('books.api.v1.urls'), name='books'),
     ]
